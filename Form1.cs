66aac6a4c734b790e8cde21667e4144a52a12bf2
Dimitrijus Bublys <72878704+DimitrijusBublys@users.noreply.github.com>
2022-05-01 01:38:22 +0300

    Add files via upload

--- Form1.cs
+++ Form1.cs
@@ -2,6 +2,8 @@ namespace projektasSlaptazodziai
 {
     public partial class Form1 : Form
     {
+        readonly string connectionString =
+                "Data Source=localhost; Initial Catalog=project; Trusted_Connection=True; TrustServerCertificate=True";
         public Form1()
         {
             InitializeComponent();
@@ -19,8 +21,6 @@ namespace projektasSlaptazodziai
 
         private void button1_Click(object sender, EventArgs e) // Master password
         {
-            string connectionString =
-                "Data Source=localhost; Initial Catalog=project; Trusted_Connection=True; TrustServerCertificate=True";
             string password = textBox1.Text;
             int dbEmpty; // Ar duomenu bazes lentele su master slaptazodziu tuscia
             if (String.IsNullOrEmpty(password))
@@ -70,64 +70,93 @@ namespace projektasSlaptazodziai
 
         }
 
-        private void button2_Click(object sender, EventArgs e) // Slaptazodzio generavimas
+        private void button2_Click(object sender, EventArgs e) // Slaptazodzio generavimas ir tikrinimas, ar prisijungta prie DB
         {
             bool emptyText = false;
-            int capitalLetters = 0;
-            int smallLetters = 0;
-            int digits = 0;
-            int specialChar = 0;
+            int capitalTimes = 0;
+            int smallTimes = 0;
+            int digitsTimes = 0;
+            int specialCharTimes = 0;
 
-            // Lauku tikrinimas(ar irasytas skaicius, ar laukas ne tuscias)
 
-            //-------------------------------------------
+            string password = textBox1.Text;
 
-            if (textBox2.Text.All(char.IsDigit) && !String.IsNullOrEmpty(textBox2.Text))
+            if (String.IsNullOrEmpty(password))
             {
-                capitalLetters = int.Parse(textBox2.Text);
+                MessageBox.Show("Áveskite slaptaþodá!");
             }
-            else
-            {
-                MessageBox.Show("Laukas Capital Letters tuðèias arba áraðytas ne skaièius!");
-                emptyText = true;
-            }
+                else
+                {
+                    int dbEmpty = Database.CheckEmptyTable(connectionString);
+                    if (dbEmpty == 0) 
+                    {
+                        MessageBox.Show("Sukurkite slaptaþodá!");
+                    }
+                    else
+                    {
+                        bool result = Database.CheckCorrectPassword(connectionString, password); // Tikriname, ar slaptazodis vis dar ivestas
+                        if (result)
+                        {
+                            // Lauku tikrinimas(ar irasytas skaicius, ar laukas ne tuscias)
 
-            if (textBox3.Text.All(char.IsDigit) && !String.IsNullOrEmpty(textBox3.Text))
-            {
-                smallLetters = int.Parse(textBox3.Text);
-            }
-            else
-            {
-                MessageBox.Show("Laukas Small Letters tuðèias arba áraðytas ne skaièius!");
-                emptyText = true;
-            }
+                            //-------------------------------------------
 
-            if (textBox4.Text.All(char.IsDigit) && !String.IsNullOrEmpty(textBox4.Text))
-            {
-                digits = int.Parse(textBox4.Text);
-            }
-            else
-            {
-                MessageBox.Show("Laukas Digits tuðèias arba áraðytas ne skaièius!");
-                emptyText = true;
-            }
+                            if (textBox2.Text.All(char.IsDigit) && !String.IsNullOrEmpty(textBox2.Text))
+                            {
+                                capitalTimes = int.Parse(textBox2.Text);
+                            }
+                            else
+                            {
+                                MessageBox.Show("Laukas Capital Letters tuðèias arba áraðytas ne skaièius!");
+                                emptyText = true;
+                            }
 
-            if (textBox4.Text.All(char.IsDigit) && !String.IsNullOrEmpty(textBox5.Text))
-            {
-                specialChar = int.Parse(textBox5.Text);
-            }
-            else
-            {
-                MessageBox.Show("Laukas Special Characters tuðèias arba áraðytas ne skaièius!");
-                emptyText = true;
-            }
+                            if (textBox3.Text.All(char.IsDigit) && !String.IsNullOrEmpty(textBox3.Text))
+                            {
+                                smallTimes = int.Parse(textBox3.Text);
+                            }
+                            else
+                            {
+                                MessageBox.Show("Laukas Small Letters tuðèias arba áraðytas ne skaièius!");
+                                emptyText = true;
+                            }
 
-            //-------------------------------------------
+                            if (textBox4.Text.All(char.IsDigit) && !String.IsNullOrEmpty(textBox4.Text))
+                            {
+                                digitsTimes = int.Parse(textBox4.Text);
+                            }
+                            else
+                            {
+                                MessageBox.Show("Laukas digitsTimes tuðèias arba áraðytas ne skaièius!");
+                                emptyText = true;
+                            }
+
+                            if (textBox4.Text.All(char.IsDigit) && !String.IsNullOrEmpty(textBox5.Text))
+                            {
+                                specialCharTimes = int.Parse(textBox5.Text);
+                            }
+
+                            else
+                            {
+                                MessageBox.Show("Laukas Special Characters tuðèias arba áraðytas ne skaièius!");
+                                emptyText = true;
+                            }
+
+                            //-------------------------------------------
 
 
-            if (!emptyText) // Jeigu visi laukai aprasyti
-            {
-                MessageBox.Show("rimtega");
+                            if (!emptyText) // Jeigu visi laukai aprasyti
+                            {
+                                string generatedPassword = PasswordGenerator.GeneratePassword(capitalTimes, smallTimes, digitsTimes, specialCharTimes);
+                                MessageBox.Show("Sugeneruotas slaptaþodis: " + generatedPassword);
+                            }
+
+                        }
+                    else
+                    {
+                        MessageBox.Show("Ávestas slaptaþodis neteisingas!");
+                    }
+                }
             }
         }
     }
--- PasswordGenerator.cs
+++ PasswordGenerator.cs
@@ -8,5 +8,76 @@ namespace projektasSlaptazodziai
 {
     internal class PasswordGenerator
     {
+
+        public static string GeneratePassword(int capitalTimes, int smallTimes, int digitsTimes, int specialCharTimes)
+        {
+            // Galimi simboliai
+
+            //-------------------------------------------
+
+            // 26 reiksmes
+            string[] capitalLetters = { "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "A", "S", "D", "F", "G", "H", "J", "K", "L", "Z", "X", "C", "V", "B", "N", "M" };
+            // 26
+            string[] smallLetters = { "q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "a", "s", "d", "f", "g", "h", "j", "k", "l", "z", "x", "c", "v", "b", "n", "m" };
+            // 10
+            string[] digits = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "0" };
+            // 24
+            string[] specialChar = { "`", "~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "|", "[", "]", "{", "}", "'", "/", "?" };
+
+            //-------------------------------------------
+
+            string generatedPassword = "";
+            int length = 0;
+            length += capitalTimes + smallTimes + digitsTimes + specialCharTimes;
+
+            Random rnd = new Random();
+
+            for(int i = 0; i < length; i++)
+            {
+                int rndFunction = rnd.Next(1, 5); // nuo 1 iki 4
+
+                if(rndFunction == 1 && capitalTimes > 0) // Didieji
+                {
+                    generatedPassword += capitalLetters[rnd.Next(0, 26)];
+                    capitalTimes--;
+                }
+                else if(rndFunction == 1 && capitalTimes == 0)
+                {
+                    i--;
+                }
+
+                if (rndFunction == 2 && smallTimes > 0) // Mazieji
+                {
+                    generatedPassword += smallLetters[rnd.Next(0, 26)];
+                    smallTimes--;
+                }
+                else if (rndFunction == 2 && smallTimes == 0)
+                {
+                    i--;
+                }
+
+                if (rndFunction == 3 && digitsTimes > 0) // Skaiciai
+                {
+                    generatedPassword += digits[rnd.Next(0, 10)];
+                    digitsTimes--;
+                }
+                else if (rndFunction == 3 && digitsTimes == 0)
+                {
+                    i--;
+                }
+
+                if (rndFunction == 4 && specialCharTimes > 0) // Simboliai
+                {
+                    generatedPassword += specialChar[rnd.Next(0, 24)];
+                    specialCharTimes--;
+                }
+                else if (rndFunction == 4 && specialCharTimes == 0)
+                {
+                    i--;
+                }
+            }
+
+            return generatedPassword;
+        } 
     }
 }
